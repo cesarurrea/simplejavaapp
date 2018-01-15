160398eba891b67730dc9b85e6b3a97537b34a6a
Cesar Urrea <cesarurrea@gmail.com>
2018-01-15 13:56:15 -0500

    Checkin inicial Simple Java App

---
+++ SimpleJavaApp/src/org/co/main/Main.java
@@ -0,0 +1,18 @@
+package org.co.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+
+		displayGreeting();
+		data.display();
+
+	}
+
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+	}
+
+}
---
+++ SimpleJavaApp/src/org/co/main/SalesData.java
@@ -0,0 +1,10 @@
+package org.co.main;
+
+public class SalesData {
+	int data[] = { 0, 4, 42 };
+
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+
+}
